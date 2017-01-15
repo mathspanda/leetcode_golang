7ceb2eca479dc007ea385517ca17fcbc5067e3e7
MATHSPANDA <mathspanda826@gmail.com>
2017-01-15 22:27:48 +0800

    67

---
+++ zigzag_conversion.go
@@ -0,0 +1,18 @@
+func convert(s string, numRows int) string {
+    result := make([]string, numRows)
+    for i := 0 ; i < len(s); {
+    	for j := 0; j < numRows && i < len(s); j++ {
+    		result[j] += string(s[i])
+    		i++
+    	}
+    	for j := numRows - 2; j > 0 && i < len(s); j-- {
+    		result[j] += string(s[i])
+    		i++
+    	}
+    }
+    resultStr := ""
+    for i := 0; i < len(result); i++ {
+    	resultStr += result[i]
+    }
+    return resultStr
+}
