26c3972466ed3f865ead49a9d9b9b2a547b83f11
MATHSPANDA <mathspanda826@gmail.com>
2017-03-08 23:15:48 +0800

    modify 38

--- combination_sum.go
+++ combination_sum.go
@@ -1,5 +1,30 @@
 package main
 
-func combinationSum(candidates []int, target int) [][]int {
+import (
+	"sort"
+)
 
+func combinationSum(candidates []int, target int) [][]int {
+	var result [][]int
+	sort.Ints(candidates)
+	combine(candidates, []int{}, 0, target, &result)
+	return result
+}
+
+func combine(candidates, temps []int, startIndex, target int, result *[][]int) {
+	if target < 0 {
+		return
+	}
+	if target == 0 {
+		real := make([]int, len(temps))
+		copy(real, temps)
+		*result = append(*result, real)
+		return
+	}
+	for i := startIndex; i < len(candidates); i++ {
+		temps = append(temps, candidates[i])
+		combine(candidates, temps, i, target-candidates[i], result)
+		temps = temps[:len(temps)-1]
+	}
+	return
 }
