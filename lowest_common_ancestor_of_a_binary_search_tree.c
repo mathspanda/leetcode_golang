1b61bc8d1931bc4f8df5315d337ad2a8cdb5aaba
MATHSPANDA <mathspanda826@gmail.com>
2017-05-06 20:17:23 +0800

    LCA 235,236

---
+++ lowest_common_ancestor_of_a_binary_search_tree.c
@@ -0,0 +1,22 @@
+struct TreeNode {
+    int val;
+    struct TreeNode *left;
+    struct TreeNode *right;
+};
+ 
+struct TreeNode* lowestCommonAncestor(struct TreeNode* root, struct TreeNode* p, struct TreeNode* q) {
+    if (root == NULL) {
+        return root;
+    }
+    if (p->val <= root->val && q->val >= root->val) {
+        return root;
+    }
+    if (p->val >= root->val && q->val <= root->val) {
+        return root;
+    }
+    if (p->val <= root->val) {
+        return lowestCommonAncestor(root->left, p, q);
+    } else {
+        return lowestCommonAncestor(root->right, p, q);
+    }
+}
